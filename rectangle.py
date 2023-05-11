fea35cd9cfe9da840a62a4e3017065ee3a3691f1
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2023-05-11 01:55:56 -0400

    Initial commit

---
+++ rectangle.py
@@ -0,0 +1,10 @@
+#import the libraries needed
+from math import pi
+
+#Must ask for customer name
+#Must ask for customer address
+#Must ask for the dimension of the room in feet
+#It should determine the cost to put the flooring in the room
+#rectangle = area = side1 * side2
+#Flooring material cost 2.00
+#Installation cost 1.50
