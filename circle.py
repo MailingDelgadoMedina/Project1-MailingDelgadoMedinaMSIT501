eb8264557a6f4a375237abf33cc1017fd66c7972
MailingDelgadoMedina <40926855+MailingDelgadoMedina@users.noreply.github.com>
2023-05-11 20:48:02 +0000

    Finished final touch of circle app

--- circle.py
+++ circle.py
@@ -1,3 +1,8 @@
+"""
+# Mailing Delgado Medina
+# N02005278
+"""
+
 # import the libraries needed
 from math import pi
 
@@ -6,25 +11,36 @@ name = input('What is the name of the customer? ')
 # Must ask for customer address
 address = input('What is the address of the customer? ')
 # Must ask for the dimension of the room in feet
-one_side_of_the_room_size = eval(input('What is the size of 1 side of the room (in feet)? '))
+diameter = eval(input('What is the diameter of the room (in feet)? '))
+
 # It should determine the cost to put the flooring in the room
-radius = one_side_of_the_room_size / 2
-# circle = area = radius^2 * pi
+
+# calculate the radious by dividing the diameter by 2
+radius = diameter / 2
+# calculate the area  area = radius^2 * pi
 area = pi * radius ** 2
-# Flooring material cost 2.00
-flooring_material_cost = 2.00
-# Estimated cost for flooring material
-total_flooring_material_cost = area * flooring_material_cost
-# Installation cost 1.50
-installation_cost = 1.50
 
-total_installation_cost = area * installation_cost
+# Flooring material cost 2.00 is beign written in upper case snake case because is a constant
+FLOORING_MATERIAL_COST = 2.00
 
+# Calculate the estimated cost for flooring material
+total_flooring_material_cost = area * FLOORING_MATERIAL_COST
+
+# Installation cost 1.50 is beign written in upper case snake case because is a constant
+INSTALLATION_COST = 1.50
+
+# Calculate the estimated installation cost
+total_installation_cost = area * INSTALLATION_COST
+
+#Calculate the total estimate
 total_estimate = total_installation_cost + total_flooring_material_cost
 
+#Estimate Receipt
 print('Estimate for ', name)
 print(address)
 
+#This will print a blank line
+print("\n")
 
 print(f'Circular room with area of {area:.2f} square feet')
 print(f'Estimated cost for flooring material is ${total_flooring_material_cost:.2f}')
